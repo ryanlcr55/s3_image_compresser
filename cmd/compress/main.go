13b5ccb5759f5535acf9881d8b237b9052538a5a
Ryan <lcrob55@gmail.com>
2022-04-05 03:27:07 +0800

    feat: get file list from S3

--- cmd/compress/main.go
+++ cmd/compress/main.go
@@ -1,9 +1,43 @@
 package main
 
-// Lists the items in the specified S3 Bucket
-//
-// Usage:
-//    go run s3_list_objects.go BUCKET_NAME
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func main() {
+	//bucket name
+	bucket := "ryan-li-practice"
+	prefix := "test"
+
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-1")},
+	)
+
+	// Create S3 service client
+	svc := s3.New(sess)
+	// Get the list of items
+	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket), Prefix: aws.String(prefix)})
+	if err != nil {
+		exitErrorf("Unable to list items in bucket %q, %v", bucket, err)
+	}
+
+	for _, item := range resp.Contents {
+		fmt.Println("Name:         ", *item.Key)
+		fmt.Println("Last modified:", *item.LastModified)
+		fmt.Println("Size:         ", *item.Size)
+		fmt.Println("Storage class:", *item.StorageClass)
+		fmt.Println("")
+	}
+	fmt.Println("Found", len(resp.Contents), "items in bucket", bucket)
+	fmt.Println("")
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/lcrob55/s3_image_compresser
 
 go 1.18
+
+require github.com/aws/aws-sdk-go v1.43.31
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
